1e064bc8b0da7e7bce11a29fd318dec3945de97d
Dylan Aspden <dhaspden@gmail.com>
2016-02-12 19:32:36 -0500

    Feat: add gulp task for building the project

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+
+gulp.task('build', () => {
+  return gulp.src([
+      'index.js',
+      '**/*.js',
+      '!**/*.spec.js',
+      '!coverage/**/*',
+      '!node_modules/**/*',
+      '!dist/**/*',
+      '!gulpfile.js'
+    ])
+    .pipe(babel({
+      presets: ['stage-0']
+    }))
+    .pipe(gulp.dest('dist'));
+});
